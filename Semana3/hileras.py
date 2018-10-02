9d75c26d5971381ce59f73f1b1fe4b9dd770ac5c
Maria Madrigal <maria.madrigal.chacon@gmail.com>
2018-10-01 20:47:15 -0600

    String manipulation examples

--- Semana3/hileras.py
+++ Semana3/hileras.py
@@ -1,1 +1,39 @@
 #Tema Hileras
+
+message = input("Digite lo que quiera: ")
+
+print(message)
+
+#len() -> Encontrar la cantidad de caracteres en una hilera
+
+print(len(message))
+
+# Indices
+
+print(message[0]) # Obtenemos el primer caracter de una hilera
+print(message[len(message) -1]) # Obtenemos el ultimo caracter de una hilera
+
+# Concatenacion de hileras
+
+hilera = "Hilera inicial"
+
+# Concatenacion simple
+
+print(hilera + " otra hilera")
+print(hilera)
+
+# Concatenacion modificando la variable
+
+hilera += ". Esto si modifica la variable hilera"
+print(hilera)
+
+# Inyectando texto
+
+otra_hilera = "Hola {}! Como esta?".format("Maria")
+print(otra_hilera)
+
+print(otra_hilera[3:10])
+print(otra_hilera[:10])
+print(otra_hilera[8:])
+
+
